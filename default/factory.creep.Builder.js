567fcec0eec7c61d1224ce694d78ba5d88d07efe
1755616537 <1755616537@qq.com>
2023-11-09 23:24:52 +0800

    no message

--- default/factory.creep.Builder.js
+++ default/factory.creep.Builder.js
@@ -1,35 +1,41 @@
 var pro = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
+	/** @param {Creep} creep **/
+	run: function(creep) {
+		creep.memory.building = true;
+		if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) { // building && 背包为空
+			// creep.memory.building = false;  // 变为 非building状态
+			creep.say('🔄 收获');
+		}
+		if (!creep.memory.building && creep.store.getFreeCapacity() == 0) { // 非building状态 && 背包满(空余为0)
+			creep.memory.building = true; // 变为 building状态
+			creep.say('🚧 建造');
+		}
 
-	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) { // building && 背包为空
-            creep.memory.building = false;  // 变为 非building状态
-            creep.say('🔄 收获');
-	    }
-	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) { // 非building状态 && 背包满(空余为0)
-	        creep.memory.building = true;  // 变为 building状态
-	        creep.say('🚧 建造');
-	    }
-
-	    if(creep.memory.building) {  // building状态的时候
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES); // 寻找建筑位
-            if(targets.length) {  // targets.length > 0  || 建筑位 > 0
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}}); // 绘制路径
-                }
-            }
-	    }
-	    else {  // 非building状态的时候， 到source旁边并采集
-            if(creep.harvest(factory.spawns.get(1)) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(factory.spawns.get(1), {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-	    }
+		if (creep.memory.building) { // building状态的时候
+			var targets = creep.room.find(FIND_CONSTRUCTION_SITES); // 寻找建筑位
+			if (targets.length) { // targets.length > 0  || 建筑位 > 0
+				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], {
+						visualizePathStyle: {
+							stroke: '#ffffff'
+						}
+					}); // 绘制路径
+				}
+			}
+		} else { // 非building状态的时候， 到source旁边并采集
+			if (creep.harvest(factory.spawns.get(1)) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(factory.spawns.get(1), {
+					visualizePathStyle: {
+						stroke: '#ffaa00'
+					}
+				});
+			}
+		}
 	},
-	length:()=>{
+	length: () => {
 		return _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 	}
 };
 
 global.factory.creep.Builder = pro;
-
