1c21da15b44fa40b81d93af608abf0ed2e1eda05
1755616537 <1755616537@qq.com>
2023-11-09 22:27:35 +0800

    no message

--- default/main.js
+++ default/main.js
@@ -4,7 +4,7 @@
 // 拓展总入口
 require("main_mount");
 
-console.log("Script Reload In Time "+Game.time+" , bucket "+Game.cpu.bucket);
+console.log("脚本重新加载时间"+Game.time+" , bucket "+Game.cpu.bucket);
 
 module.exports.loop = function() {
 	controller.creep.run();
