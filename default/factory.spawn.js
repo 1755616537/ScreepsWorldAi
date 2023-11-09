fa0b8cb8d1dd88fe02dae7bc17cc4cdd6e250f19
1755616537 <1755616537@qq.com>
2023-11-09 22:46:03 +0800

    no message

--- default/factory.spawn.js
+++ default/factory.spawn.js
@@ -2,16 +2,16 @@ global.factory.spawns = {
 	get: (sequence) => {
 		switch (sequence) {
 			case 1:
-				Game.spawns[pathData.SpawnName1];
+				return Game.spawns[pathData.SpawnName1];
 				break;
 			case 2:
-				Game.spawns[pathData.SpawnName2];
+				return Game.spawns[pathData.SpawnName2];
 				break;
 			case 3:
-				Game.spawns[pathData.SpawnName3];
+				return Game.spawns[pathData.SpawnName3];
 				break;
 			default:
-				Game.spawns[pathData.SpawnName1];
+				return Game.spawns[pathData.SpawnName1];
 		}
 	}
 }
