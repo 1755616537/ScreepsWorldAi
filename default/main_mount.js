ad06de114e9612d2b7507ff82761e8e94162c4e5
1755616537 <1755616537@qq.com>
2023-11-09 22:17:21 +0800

    no message

--- default/main_mount.js
+++ default/main_mount.js
@@ -2,17 +2,15 @@
 // 全局数据
 require('pathData');
 // -------------------------------
-// require('role.harvester');
-// require('role.upgrader');
-// require('role.builder');
 // factory
-require('factory.creep.Harvester')
-require('factory.creep.Upgrader')
-require('factory.creep.Builder')
 // 挂载 creep 管理模块
 require('factory.creep')
 // 挂载 creep 拓展
-require('factory.creep.mount')
+// require('factory.creep.mount')
+
+require('factory.creep.Harvester')
+require('factory.creep.Upgrader')
+require('factory.creep.Builder')
 // -------------------------------
 // controller
 require('controller.creep')
