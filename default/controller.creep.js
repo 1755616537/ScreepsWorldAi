54c4699fc20456315e6d9c5a2943295db139470d
1755616537 <1755616537@qq.com>
2023-11-12 00:21:22 +0800

    no message

--- default/controller.creep.js
+++ default/controller.creep.js
@@ -4,21 +4,22 @@ global.controller.creep = {
 
 		// 遍历所有 creep 并执行上文中拓展的 work 方法
 		// Object.values(Game.creeps).forEach(creep => creep.work())
-		
+
 		// 清理内存
 		factory.creep.CleanMemory();
 
-		let harvests = factory.creep.Harvest.ALL();
-		let upgraders = factory.creep.Upgrader.ALL();
-		let builders = factory.creep.Builder.ALL();
-		let carriers = factory.creep.Carrier.ALL();
+		const harvests = factory.creep.Harvest.ALL();
+		const upgraders = factory.creep.Upgrader.ALL();
+		const builders = factory.creep.Builder.ALL();
+		const carriers = factory.creep.Carrier.ALL();
 		//console.log('Harvesters: ' + harvesters.length);
 		const controller_level = factory.spawns.get(1).room.controller.level; // 查看控制器等级
 		//console.log('controller:' + factory.spawns.get(1).room.controller.level)
-		
-		
-		// 孵化过程可视化
-		if (factory.spawns.get(1).spawning) { 
+
+
+		// 母巢 (spawn) 是否正在孵化一个新的 creep
+		if (factory.spawns.get(1).spawning) {
+			// 孵化，过程可视化
 			let spawningCreep = Game.creeps[factory.spawns.get(1).spawning.name];
 			factory.spawns.get(1).room.visual.text(
 				'孵化🛠️' + spawningCreep.memory.role,
@@ -31,52 +32,59 @@ global.controller.creep = {
 			// 生产 harvester
 			if (harvests.length < 2) {
 				let newName = globalData.harvest + Game.time;
-				console.log('生成新的 采集者: ' + newName);
-				factory.spawns.get(1).spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
+				let returnData = factory.spawns.get(1).spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
 					memory: {
 						role: globalData.harvest
 					}
 				});
+				if (returnData == OK) {
+					console.log('生成新的 采集者: ' + newName);
+				}
 			}
 
 			// 生产 carrier
-			if (harvests.length >= 2 &&carriers.length < 2) {
+			if (harvests.length >= 2 && carriers.length < 2) {
 				let newName = globalData.carrier + Game.time;
-				console.log('生成新的 运输者:' + newName);
-				factory.spawns.get(1).spawnCreep([WORK, CARRY, CARRY, MOVE], newName, {
+				let returnData = factory.spawns.get(1).spawnCreep([WORK, CARRY, CARRY, MOVE], newName, {
 					memory: {
 						role: globalData.carrier
 					}
 				});
+				if (returnData == OK) {
+					console.log('生成新的 运输者:' + newName);
+				}
 			}
 
 			// 生产 upgrader
 			if (harvests.length >= 2 && upgraders.length < 2) {
 				let newName = globalData.upgrader + Game.time;
-				console.log('生成新的 升级者: ' + newName);
-				factory.spawns.get(1).spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
+				let returnData = factory.spawns.get(1).spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
 					memory: {
 						role: globalData.upgrader
 					}
 				});
+				if (returnData == OK) {
+					console.log('生成新的 升级者: ' + newName);
+				}
 			}
 
 			// 生产 builder
 			if (controller_level >= 2 && builders.length < 2) {
 				let newName = globalData.builder + Game.time;
-				console.log('生成新的 建造者:' + newName);
-				factory.spawns.get(1).spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
+				let returnData = factory.spawns.get(1).spawnCreep([WORK, WORK, CARRY, MOVE], newName, {
 					memory: {
 						role: globalData.builder
 					}
 				});
+				if (returnData == OK) {
+					console.log('生成新的 建造者:' + newName);
+				}
 			}
-
-
 		}
 
-		for (var name in Game.creeps) {
-			var creep = Game.creeps[name];
+		// 事件管理
+		for (let name in Game.creeps) {
+			let creep = Game.creeps[name];
 			if (creep.memory.role == globalData.harvest) {
 				factory.creep.Harvest.run(creep);
 			}
