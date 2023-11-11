b1fb4628f02168d38fc44383763d910a85970e81
1755616537 <1755616537@qq.com>
2023-11-11 17:23:47 +0800

    no message

--- default/factory.creep.Carrier.js
+++ default/factory.creep.Carrier.js
@@ -5,26 +5,11 @@ var pro = {
 	/** @param {Creep} creep **/
 	run: function(creep) {
 		if (creep.store.getFreeCapacity() > 0) { // 背包未满
-			let targets = _.compact(
-				// 所有掉落的资源
-				// creep.room.find(FIND_DROPPED_RESOURCES),
-				// 所有建筑
-				creep.room.find(FIND_STRUCTURES, {
-					filter: (structure) => {
-						// 找出有储存能量的container搬运
-						return (structure.structureType == STRUCTURE_CONTAINER) &&
-							structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
-					}
-				}),
-				// 所有墓碑
-				creep.room.find(FIND_TOMBSTONES),
-				// 所有废墟
-				// creep.room.find(FIND_RUINS)
-			);
-
+			// 所有掉落的资源
+			let targets = creep.room.find(FIND_DROPPED_RESOURCES);
 			if (targets.length > 0) {
-				// 从建筑(structure)中拿取资源
-				if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				// 捡起一个物品 (如捡起一些能量)
+				if (creep.pickup(targets[0]) == ERR_NOT_IN_RANGE) {
 					// 向目标移动
 					creep.moveTo(targets[0], {
 						visualizePathStyle: {
@@ -32,7 +17,35 @@ var pro = {
 						}
 					});
 				}
+			} else {
+				let targets = _.compact(
+					// 所有建筑
+					creep.room.find(FIND_STRUCTURES, {
+						filter: (structure) => {
+							// 找出有储存能量的container搬运
+							return (structure.structureType == STRUCTURE_CONTAINER) &&
+								structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
+						}
+					}),
+					// 所有墓碑
+					creep.room.find(FIND_TOMBSTONES),
+					// 所有废墟
+					// creep.room.find(FIND_RUINS)
+				);
+
+				if (targets.length > 0) {
+					// 从建筑(structure)中拿取资源
+					if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+						// 向目标移动
+						creep.moveTo(targets[0], {
+							visualizePathStyle: {
+								stroke: '#ffaa00'
+							}
+						});
+					}
+				}
 			}
+
 		} else {
 			// 找出需要补充能量的建筑
 			var targets = creep.room.find(FIND_STRUCTURES, {
