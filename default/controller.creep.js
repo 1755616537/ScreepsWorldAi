4864f8d449e82b650c5fd0ce7552cfbe425708e6
1755616537 <1755616537@qq.com>
2023-11-12 22:32:14 +0800

    优先级顺序生产 每种保持最低1个

--- default/controller.creep.js
+++ default/controller.creep.js
@@ -35,30 +35,40 @@ global.controller.creep = {
 
 			// 最少采集2个
 			if (harvests.length >= 2) {
-				// 生产 升级
-				if (upgraders.length < globalData.creepConfigs.upgrader.number) {
-					factory.creep.addUpgrader(upgraders);
+				// 优先级顺序生产 每种保持最低1个
+				let priority;
+				if (upgraders.length < 1) {
+					priority = 'upgrader';
+				} else if (builders.length < 1) {
+					priority = 'builder';
+				} else if (carriers.length < 1) {
+					priority = 'carrier';
+				} else if (repairers.length < 1) {
+					priority = 'repairer';
 				}
+				if (priority) {
+					switch (priority) {
+						case 'upgrader':
+							addUpgrader(upgraders);
+							break;
+						case 'builder':
+							addBuilder(builders, controller_level);
+							break;
+						case 'carrier':
+							addCarrier(carriers);
+							break;
+						case 'repairer':
+							addRepairer(repairers);
+							break;
+						default:
+					}
 
-				// 生产 建造 前提控制器2等级
-				if (controller_level >= 2 && builders.length < globalData.creepConfigs.builder.number) {
-					factory.creep.addBuilder(builders);
-				}
-				
-				// 生产 运输
-				if (carriers.length < globalData.creepConfigs.carrier.number) {
-					// 拥有CONTAINER才生产
-					const builds = factory.spawns.get(1).room.find(FIND_MY_STRUCTURES, {
-						filter: {
-							structureType: STRUCTURE_CONTAINER
-						}
-					});
-					if (builds.length > 0) factory.creep.addCarrier(carriers);
-				}
-
-				// 生产 维修
-				if (repairers.length < globalData.creepConfigs.repairer.number) {
-					factory.creep.addRepairer(repairers);
+				} else {
+					// 默认顺序生产
+					addUpgrader(upgraders);
+					addBuilder(builders, controller_level);
+					addCarrier(carriers);
+					addRepairer(repairers);
 				}
 			}
 		}
@@ -82,5 +92,39 @@ global.controller.creep = {
 				factory.creep.Repairer.run(creep);
 			}
 		}
+	}
+}
+
+function addUpgrader(upgraders) {
+	// 生产 升级
+	if (upgraders.length < globalData.creepConfigs.upgrader.number) {
+		return factory.creep.addUpgrader(upgraders);
+	}
+}
+
+function addBuilder(builders, controller_level) {
+	// 生产 建造 前提控制器2等级
+	if (controller_level >= 2 && builders.length < globalData.creepConfigs.builder.number) {
+		return factory.creep.addBuilder(builders);
+	}
+}
+
+function addCarrier(carriers) {
+	// 生产 运输
+	if (carriers.length < globalData.creepConfigs.carrier.number) {
+		// 拥有CONTAINER才生产
+		const builds = factory.spawns.get(1).room.find(FIND_MY_STRUCTURES, {
+			filter: {
+				structureType: STRUCTURE_CONTAINER
+			}
+		});
+		if (builds.length > 0) return factory.creep.addCarrier(carriers);
+	}
+}
+
+function addRepairer(repairers) {
+	// 生产 维修
+	if (repairers.length < globalData.creepConfigs.repairer.number) {
+		return factory.creep.addRepairer(repairers);
 	}
 }
