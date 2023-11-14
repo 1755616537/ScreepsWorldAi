6d3e9bdd137fefa06f84b2600da28b34e9a47e23
1755616537 <1755616537@qq.com>
2023-11-14 23:02:20 +0800

    no message

--- default/controller.Tower.js
+++ default/controller.Tower.js
@@ -3,11 +3,46 @@ global.controller.Tower = {
 		var tower = Game.getObjectById('65533272618670f5499be7a9');
 		if (tower) {
 			// 维修
-			var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-				filter: (structure) => structure.hits < structure.hitsMax
+			let targets = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_CONTAINER) &&
+						structure.hits < structure.hitsMax;
+				}
 			});
-			if (closestDamagedStructure) {
-				tower.repair(closestDamagedStructure);
+			// 可通行墙壁
+			if (targets.length < 1) {
+				targets = creep.room.find(FIND_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_RAMPART) &&
+							structure.hits < structure.hitsMax;
+					}
+				});
+			}
+			// 路
+			if (targets.length < 1) {
+				targets = creep.room.find(FIND_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_ROAD) &&
+							structure.hits < structure.hitsMax;
+					}
+				});
+			}
+			// 墙壁
+			if (targets.length < 1) {
+				targets = creep.room.find(FIND_STRUCTURES, {
+					filter: (structure) => {
+						return (structure.structureType == STRUCTURE_WALL) &&
+							structure.hits < structure.hitsMax;
+					}
+				});
+			}
+			if (targets.length < 1) {
+				targets = creep.room.find(FIND_STRUCTURES, {
+					filter: (structure) => structure.hits < structure.hitsMax
+				});
+			}
+			if (targets.length > 0) {
+				tower.repair(targets[0]);
 			}
 			// 攻击
 			var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
