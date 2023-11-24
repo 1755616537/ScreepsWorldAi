d341e7aeb51c8cd734bdb7f1fa9fe1890595fdee
1755616537 <1755616537@qq.com>
2023-11-25 00:28:12 +0800

    no message

--- default/class.constructor.task.js
+++ default/class.constructor.task.js
@@ -1,11 +1,11 @@
 class Task {
 
-	static TaskType_harvest = 1;
-	static TaskType_upgrader = 2;
-	static TaskType_builder = 3;
-	static TaskType_carrier = 4;
-	static TaskType_repairer = 5;
-	static TaskType_defender = 6;
+	// static TaskType_harvest = 1;
+	// static TaskType_upgrader = 2;
+	// static TaskType_builder = 3;
+	// static TaskType_carrier = 4;
+	// static TaskType_repairer = 5;
+	// static TaskType_defender = 6;
 
 	constructor(taskName, taskType, executeCreep) {
 		// 任务名称
