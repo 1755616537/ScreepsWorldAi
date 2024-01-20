55e1448632d7427ad2e655dbe499e934b893ab7d
1755616537 <1755616537@qq.com>
2024-01-21 00:59:42 +0800

    no message

--- default/initialization.js
+++ default/initialization.js
@@ -9,24 +9,25 @@ global.initialization = {
 		// 矿区块初始化
 
 		// 控制器块初始化
+		// 9*9范围自动生成CONTAINER
 		let room = Game.rooms[globalData.roomName1];
 		let pos = room.controller.pos;
 		let found = _.filter(room.lookAtArea(pos.y - 1, pos.x - 1, pos.y + 1,
 				pos.x + 1, true), (f) =>
 			(f.terrain == 'plain' || f.terrain == 'swamp') &&
 			room.lookAt(f.pos)[0].structureType != STRUCTURE_WALL);
-			if (found.length > 1) {
-				// 是否已经存在CONTAINER
-				if(_.filter(found,(f) =>room.lookAt(f.pos)[0].structureType == STRUCTURE_CONTAINER)<1){
-					let x = found[0].x;
-					let y = found[0].y;
-					// 指定位置创建一个新的 ConstructionSite
-					let returnData = Game.rooms[globalData.roomName1]
-						.createConstructionSite(x, y, STRUCTURE_CONTAINER);
-					if (returnData != OK) clog(x, y, '自动建造对应数量的CONTAINER ',
-						returnData);
-				}
+		if (found.length > 1) {
+			// 是否已经存在CONTAINER
+			if (_.filter(found, (f) => room.lookAt(f.pos)[0].structureType == STRUCTURE_CONTAINER).length < 1) {
+				let x = found[0].x;
+				let y = found[0].y;
+				// 指定位置创建一个新的 ConstructionSite
+				let returnData = Game.rooms[globalData.roomName1]
+					.createConstructionSite(x, y, STRUCTURE_CONTAINER);
+				if (returnData != OK) clog(x, y, '自动建造对应数量的CONTAINER ',
+					returnData);
 			}
-			
+		}
+
 	},
 }
