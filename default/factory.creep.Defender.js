1ce1b353cf5fb40e4ad6ede45389696bfc47f4e4
1755616537 <1755616537@qq.com>
2024-01-20 20:35:43 +0800

    no message

--- default/factory.creep.Defender.js
+++ default/factory.creep.Defender.js
@@ -1,3 +1,16 @@
 global.factory.creep.Defender = {
-	run: (creep) => {}
+	run: (creep) => {},
+	ALLNearDefender: (...e) => {
+		return allNearDefender(...e);
+	},
+	ALLFarDefender: (...e) => {
+		return allFarDefender(...e);
+	}
+}
+
+function allNearDefender() {
+	return _.filter(Game.creeps, (creep) => creep.memory.role == globalData.nearDefender);
+}
+function allFarDefender() {
+	return _.filter(Game.creeps, (creep) => creep.memory.role == globalData.farDefender);
 }
