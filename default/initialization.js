804daed77c68ec21f94b6115937fe134ba42f9c5
1755616537 <1755616537@qq.com>
2024-01-21 01:21:37 +0800

    no message

--- default/initialization.js
+++ default/initialization.js
@@ -9,26 +9,44 @@ global.initialization = {
 		// 矿区块初始化
 
 		// 控制器块初始化
-		// 9*9范围自动生成CONTAINER
-		let room = Game.rooms[globalData.roomName1];
-		let pos = room.controller.pos;
-		let found = room.lookAtArea(pos.y - 1, pos.x - 1, pos.y + 1,
-			pos.x + 1, true);
-		let foundFilter = _.filter(found, (f) =>
-			(f.terrain == 'plain' || f.terrain == 'swamp') &&
-			room.lookAt(f.pos)[0].structureType != STRUCTURE_WALL);
-		if (foundFilter.length > 1) {
-			// 是否已经存在CONTAINER
-			if (_.filter(found, (f) => f.type == LOOK_CONSTRUCTION_SITES || room.lookAt(f.pos)[0].structureType ==
-					STRUCTURE_CONTAINER).length  < 1) {
-				let x = foundFilter[0].x;
-				let y = foundFilter[0].y;
-				// 指定位置创建一个新的 ConstructionSite
-				let returnData = Game.rooms[globalData.roomName1]
-					.createConstructionSite(x, y, STRUCTURE_CONTAINER);
-				if (returnData != OK) clog(x, y, '自动建造对应数量的CONTAINER ',
-					returnData);
-			}
-		}
+		controllerPiece(1);
 	},
+}
+
+// 控制器块初始化
+function controllerPiece(spawn = 1) {
+	let room;
+	switch (sequence) {
+		case 1:
+			room = Game.rooms[globalData.roomName1];
+			break;
+		case 2:
+			room = Game.rooms[globalData.roomName2];
+			break;
+		case 3:
+			room = Game.rooms[globalData.roomName3];
+			break;
+		default:
+			room = Game.rooms[globalData.roomName1];
+	}
+	// 9*9范围自动生成CONTAINER
+	let pos = room.controller.pos;
+	let found = room.lookAtArea(pos.y - 1, pos.x - 1, pos.y + 1,
+		pos.x + 1, true);
+	let foundFilter = _.filter(found, (f) =>
+		(f.terrain == 'plain' || f.terrain == 'swamp') &&
+		room.lookAt(f.pos)[0].structureType != STRUCTURE_WALL);
+	if (foundFilter.length > 1) {
+		// 是否已经存在CONTAINER
+		if (_.filter(found, (f) => f.type == LOOK_CONSTRUCTION_SITES || room.lookAt(f.pos)[0].structureType ==
+				STRUCTURE_CONTAINER).length < 1) {
+			let x = foundFilter[0].x;
+			let y = foundFilter[0].y;
+			// 指定位置创建一个新的 ConstructionSite
+			let returnData = Game.rooms[globalData.roomName1]
+				.createConstructionSite(x, y, STRUCTURE_CONTAINER);
+			if (returnData != OK) clog(x, y, '自动建造对应数量的CONTAINER ',
+				returnData);
+		}
+	}
 }
