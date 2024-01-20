5967933c2a60b1b0aa5ef8c20251ae1927c66fb8
1755616537 <1755616537@qq.com>
2024-01-21 00:58:52 +0800

    no message

--- default/initialization.js
+++ default/initialization.js
@@ -15,15 +15,18 @@ global.initialization = {
 				pos.x + 1, true), (f) =>
 			(f.terrain == 'plain' || f.terrain == 'swamp') &&
 			room.lookAt(f.pos)[0].structureType != STRUCTURE_WALL);
-		if (found.length > 1) {
-			let x = found[0].x;
-			let y = found[0].y;
-			// 指定位置创建一个新的 ConstructionSite
-			let returnData = Game.rooms[globalData.roomName1]
-				.createConstructionSite(x, y, STRUCTURE_CONTAINER);
-			if (returnData != OK) clog(x, y, '自动建造对应数量的CONTAINER ',
-				returnData);
-		}
-
+			if (found.length > 1) {
+				// 是否已经存在CONTAINER
+				if(_.filter(found,(f) =>room.lookAt(f.pos)[0].structureType == STRUCTURE_CONTAINER)<1){
+					let x = found[0].x;
+					let y = found[0].y;
+					// 指定位置创建一个新的 ConstructionSite
+					let returnData = Game.rooms[globalData.roomName1]
+						.createConstructionSite(x, y, STRUCTURE_CONTAINER);
+					if (returnData != OK) clog(x, y, '自动建造对应数量的CONTAINER ',
+						returnData);
+				}
+			}
+			
 	},
 }
