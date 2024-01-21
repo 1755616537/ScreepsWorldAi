5db76b6c615641639ba1013078a541c5dbfc720b
1755616537 <1755616537@qq.com>
2024-01-21 12:28:52 +0800

    no message

--- default/initialization.js
+++ default/initialization.js
@@ -39,14 +39,8 @@ function controllerPiece(spawn = 1) {
 		room.lookAt(f.pos)[0].structureType != STRUCTURE_WALL);
 	if (foundFilter.length > 1) {
 		// 是否已经存在CONTAINER
-		let found2 = _.filter(found, (f) => {
-			console.log(JSON.stringify(f))
-			let found3 = room.lookForAt(LOOK_STRUCTURES, f.pos);
-			console.log(JSON.stringify(found3))
-			console.log('----------------------2')
-			return f.type == LOOK_CONSTRUCTION_SITES || (f.type == LOOK_STRUCTURES && f.structureType ==
-				STRUCTURE_CONTAINER);
-		})
+		let found2 = _.filter(found, (f) => f.type == LOOK_CONSTRUCTION_SITES || (f.type == LOOK_STRUCTURES && f
+			.structureType == STRUCTURE_CONTAINER))
 		console.log(JSON.stringify(found2))
 		if (found2.length < 1) {
 			let x = foundFilter[0].x;
