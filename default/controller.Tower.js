3b8ff2494763e8f6c609d4873b0a37d7d94100ea
1755616537 <1755616537@qq.com>
2024-01-23 01:00:55 +0800

    全部房间Tower自动攻击，维修

--- default/controller.Tower.js
+++ default/controller.Tower.js
@@ -1,71 +1,68 @@
 global.controller.Tower = {
 	run: () => {
-		var tower = Game.getObjectById('65533272618670f5499be7a9');
-		if (tower) {
-			// 攻击
-			var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-			if (closestHostile) {
-				tower.attack(closestHostile);
-				return
-			}
-
-			// 维修
-			let targets = tower.room.find(FIND_STRUCTURES, {
+		_.forEach(Game.rooms, room => {
+			const targets = room.find(FIND_MY_STRUCTURES, {
 				filter: (structure) => {
-					return (structure.structureType == STRUCTURE_CONTAINER) &&
-						structure.hits < structure.hitsMax;
+					return structure.structureType == STRUCTURE_TOWER;
 				}
 			});
-			// 可通行墙壁
-			if (targets.length < 1) {
-				targets = tower.room.find(FIND_STRUCTURES, {
-					filter: (structure) => {
-						return (structure.structureType == STRUCTURE_RAMPART) &&
-							structure.hits < structure.hitsMax;
-					}
+			if (targets.length) {
+				_.forEach(targets, target => {
+					work(target);
 				});
 			}
-			// 路
-			if (targets.length < 1) {
-				targets = tower.room.find(FIND_STRUCTURES, {
-					filter: (structure) => {
-						return (structure.structureType == STRUCTURE_ROAD) &&
-							structure.hits < structure.hitsMax;
-					}
-				});
-			}
-			// 墙壁
-			if (targets.length < 1) {
-				targets = tower.room.find(FIND_STRUCTURES, {
-					filter: (structure) => {
-						return (structure.structureType == STRUCTURE_WALL) &&
-							structure.hits < structure.hitsMax;
-					}
-				});
-			}
-			if (targets.length < 1) {
-				targets = tower.room.find(FIND_STRUCTURES, {
-					filter: (structure) => structure.hits < structure.hitsMax
-				});
-			}
-			if (targets.length > 0) {
-				tower.repair(targets[0]);
-			}
-		}
+		})
 	}
 }
 
-function defendRoom(roomSequence) {
-	var hostiles = factory.room.get(roomSequence).find(FIND_HOSTILE_CREEPS);
-	if (hostiles.length > 0) {
-		var username = hostiles[0].owner.username;
-		Game.notify(`User ${username} spotted in room ${roomSequence}`);
-		var towers = factory.room.get(roomSequence).find(
-			FIND_MY_STRUCTURES, {
-				filter: {
-					structureType: STRUCTURE_TOWER
-				}
-			});
-		towers.forEach(tower => tower.attack(hostiles[0]));
+function work(tower) {
+	// 攻击
+	var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+	if (closestHostile) {
+		tower.attack(closestHostile);
+		return
+	}
+
+	// 维修
+	let targets = tower.room.find(FIND_STRUCTURES, {
+		filter: (structure) => {
+			return (structure.structureType == STRUCTURE_CONTAINER) &&
+				structure.hits < structure.hitsMax;
+		}
+	});
+	// 可通行墙壁
+	if (targets.length < 1) {
+		targets = tower.room.find(FIND_STRUCTURES, {
+			filter: (structure) => {
+				return (structure.structureType == STRUCTURE_RAMPART) &&
+					structure.hits < structure.hitsMax;
+			}
+		});
+	}
+	// 路
+	if (targets.length < 1) {
+		targets = tower.room.find(FIND_STRUCTURES, {
+			filter: (structure) => {
+				return (structure.structureType == STRUCTURE_ROAD) &&
+					structure.hits < structure.hitsMax;
+			}
+		});
+	}
+	// 墙壁
+	if (targets.length < 1) {
+		targets = tower.room.find(FIND_STRUCTURES, {
+			filter: (structure) => {
+				return (structure.structureType == STRUCTURE_WALL) &&
+					structure.hits < structure.hitsMax;
+			}
+		});
+	}
+	if (targets.length < 1) {
+		targets = tower.room.find(FIND_STRUCTURES, {
+			filter: (structure) => structure.hits < structure.hitsMax
+		});
+	}
+	if (targets.length > 0) {
+		tower.repair(targets[0]);
 	}
 }
