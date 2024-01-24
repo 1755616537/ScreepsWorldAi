9e5a27c20c9f6872b45c77f354c906bdb485ac27
1755616537 <1755616537@qq.com>
2024-01-24 18:20:42 +0800

    no message

--- default/initialization.js
+++ default/initialization.js
@@ -66,10 +66,12 @@ function controllerPiece(spawnSequence = 1) {
 		let foundFilter = _.filter(found, (f) =>
 			(f.terrain == 'plain' || f.terrain == 'swamp') &&
 			(f.type == LOOK_STRUCTURES ? f.structure.structureType != STRUCTURE_WALL : true));
+			console.log(foundFilter.length)
 		if (foundFilter.length > 0) {
 			// 是否已经存在CONTAINER或在建的CONTAINER
 			let found2 = _.filter(found, (f) => f.type == LOOK_CONSTRUCTION_SITES || (f.type == LOOK_STRUCTURES && f
 				.structure.structureType == STRUCTURE_CONTAINER));
+				console.log(found2.length)
 			if (found2.length < 1) {
 				let x = foundFilter[0].x;
 				let y = foundFilter[0].y;
