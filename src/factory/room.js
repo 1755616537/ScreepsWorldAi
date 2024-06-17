cba24ca5423e5c71dcd7bab3c754846a250e73d6
1755616537 <1755616537@qq.com>
2024-06-17 10:50:22 +0800

    修改factory room,spawn

--- src/factory/room.js
+++ src/factory/room.js
@@ -1,24 +1,29 @@
-import factory_spawn from "../factory/spawn.js";
-import factory_room from "../factory/room.js";
-
 export default {
-    nameGet: (name) => {
-        return Game.rooms[name];
-    },
-    sequenceGet: (sequence) => {
-        return Game.rooms[factory_room.sequenceGetName(sequence)];
-    },
-    nameGetSequence: (name) => {
-        let sequence = _.findIndex(globalData.rooms, (room) => room.name == name)
-        if (sequence == -1) {
-            return 1;
-        }
-        return sequence + 1;
-    },
-    sequenceGetName: (sequence) => {
-        if (sequence > globalData.rooms.length || sequence < 1) {
-            return globalData.rooms[0].name;
-        }
-        return globalData.rooms[sequence - 1].name;
+    nameGet: nameGet,
+    sequenceGet: sequenceGet,
+    nameGetSequence: nameGetSequence,
+    sequenceGetName: sequenceGetName
+}
+
+function nameGet(name) {
+    return Game.rooms[name];
+}
+
+function sequenceGet(sequence) {
+    return Game.rooms[sequenceGetName(sequence)];
+}
+
+function nameGetSequence(name) {
+    let sequence = _.findIndex(globalData.rooms, (room) => room.name == name)
+    if (sequence == -1) {
+        return 1;
     }
+    return sequence + 1;
+}
+
+function sequenceGetName(sequence) {
+    if (sequence > globalData.rooms.length || sequence < 1) {
+        return globalData.rooms[0].name;
+    }
+    return globalData.rooms[sequence - 1].name;
 }
--- src/factory/spawn.js
+++ src/factory/spawn.js
@@ -1,43 +1,53 @@
-import factory_spawn from "../factory/spawn.js";
-import factory_room from "../factory/room.js";
-
 export default {
-// 获取全部基地名称数组
-    getNameAllArray: () => {
-        return _.chain(globalData.rooms)
-            .map('spawns') // 获取各房间的spawns数组
-            .flatten() // 将嵌套的spawns数组扁平化为一个单一数组
-            .map('name') // 映射每个spawn对象的name属性
-            .value(); // 获取最终处理的结果数组
-    },
-    nameGet: (name) => {
-        return Game.spawns[name];
-    },
-    sequenceGet: (sequence) => {
-        return Game.spawns[factory_spawn.sequenceGetName(sequence)];
-    },
-    nameGetSequence: (name) => {
-        let spawnNameAllArray = factory_spawn.getNameAllArray();
-        let sequence = _.indexOf(spawnNameAllArray, name);
-        if (sequence == -1) {
-            return 1;
-        }
-        return sequence + 1;
-    },
-    sequenceGetName: (sequence) => {
-        let spawnNameAllArray = factory_spawn.getNameAllArray();
-        if (sequence > spawnNameAllArray.length || sequence < 1) {
-            return spawnNameAllArray[0];
-        }
-        return spawnNameAllArray[sequence - 1];
-    },
-    // 基地序号获取房间序号
-    sequenceGetRoomSequence: (sequence) => {
-        let roomName = factory_spawn.sequenceGet(sequence).room.name;
-        return factory_room.nameGetSequence(roomName);
-    },
-    // 基地名称获取房间名称
-    nameGetRoomName: (name) => {
-        return factory_spawn.nameGet(name).room.name;
+    getNameAllArray: getNameAllArray,
+    nameGet: nameGet,
+    sequenceGet: sequenceGet,
+    nameGetSequence: nameGetSequence,
+    sequenceGetName: sequenceGetName,
+    sequenceGetRoomSequence: sequenceGetRoomSequence,
+    nameGetRoomName: nameGetRoomName
+}
+
+function getNameAllArray() {
+    return _.chain(globalData.rooms)
+        .map('spawns') // 获取各房间的spawns数组
+        .flatten() // 将嵌套的spawns数组扁平化为一个单一数组
+        .map('name') // 映射每个spawn对象的name属性
+        .value(); // 获取最终处理的结果数组
+}
+
+function nameGet(name) {
+    return Game.spawns[name];
+}
+
+function sequenceGet(sequence) {
+    return Game.spawns[sequenceGetName(sequence)];
+}
+
+function nameGetSequence(name) {
+    let spawnNameAllArray = getNameAllArray();
+    let sequence = _.indexOf(spawnNameAllArray, name);
+    if (sequence == -1) {
+        return 1;
     }
+    return sequence + 1;
+}
+
+function sequenceGetName(sequence) {
+    let spawnNameAllArray = getNameAllArray();
+    if (sequence > spawnNameAllArray.length || sequence < 1) {
+        return spawnNameAllArray[0];
+    }
+    return spawnNameAllArray[sequence - 1];
+}
+
+// 基地序号获取房间序号
+function sequenceGetRoomSequence(sequence) {
+    let roomName = sequenceGet(sequence).room.name;
+    return nameGetSequence(roomName);
+}
+
+// 基地名称获取房间名称
+function nameGetRoomName(name) {
+    return nameGet(name).room.name;
 }
