7445785b8131de233852dff0c2d84ebc7cc4cffa
1755616537 <1755616537@qq.com>
2024-06-19 03:28:57 +0800

    no message

--- src/factory/creep/Harvest.js
+++ src/factory/creep/Harvest.js
@@ -129,7 +129,6 @@ export default {
                                             }
                                         }
                                     }
-                                    ;
                                 }
                                 x++;
                             }
