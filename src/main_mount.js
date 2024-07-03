003880af923e64a47b42b5fae2770a476fb3a491
1755616537 <1755616537@qq.com>
2024-07-03 19:52:31 +0800

    工具集添加，建筑缓存，资源显示

--- src/main_mount.js
+++ src/main_mount.js
@@ -14,6 +14,9 @@ import "./utils/log.js"
 // 工具集
 import "./utils/utils.js";
 import "./utils/超级移动优化hotfix 0.9.4.js";
+import "./utils/极致建筑缓存 v1.4.3.js";
+// 资源显示
+import "./utils/helper_roomResource.js";
 // -------------------------------工厂模块
 // -------------------------------控制器
 // -------------------------------默认只执行一次的初始化程序
