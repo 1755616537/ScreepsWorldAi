cb6f6585d56cbb84cd7d3b1d10f0e5facc543f7c
1755616537 <1755616537@qq.com>
2024-07-04 23:33:29 +0800

    no message

--- src/factory/task.js
+++ src/factory/task.js
@@ -1,28 +1,29 @@
 import Throw from "../utils/Throw.js";
 
-function Aask() {
-    this.list = []
-}
-
-Aask.prototype.add = function (val) {
-    if (!val) return false;
-    this.list.push(val);
-    return true;
-}
-
-Aask.prototype.remove = function (index) {
-    if (!index) {
-        this.list.pop();
-    } else {
-        this.list.splice(index, 1);
+class Aask {
+    constructor() {
+        this.list = []
     }
-    return true;
-}
 
-Aask.prototype.get = function (index) {
-    if (this.list.length < 1) Throw.Error('任务列表为空');
-    if (!index) return this.list[0];
-    return this.list[index];
-}
+    add = function (val) {
+        if (!val) return false;
+        this.list.push(val);
+        return true;
+    }
 
+    remove = function (index) {
+        if (!index) {
+            this.list.pop();
+        } else {
+            this.list.splice(index, 1);
+        }
+        return true;
+    }
+
+    get = function (index) {
+        if (this.list.length < 1) Throw.Error('任务列表为空');
+        if (!index) return this.list[0];
+        return this.list[index];
+    }
+}
 export default Aask
