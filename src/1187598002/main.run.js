25e6f1250972259d9be9974da11e9932b91a5c12
1755616537 <1755616537@qq.com>
2024-07-06 12:26:34 +0800

    1187598002入口更改

---
+++ src/1187598002/main.run.js
@@ -0,0 +1,24 @@
+//var systemInitialize = require('./system/system.initialize'); //初始化 - 系统模块
+//var systemArtificialTask = require('./system/system.artificialTask'); //人工任务队列-系统模块
+
+//import { systemInitialize } from './system/system.initialize' //初始化 - 系统模块
+
+import { systemInitialize } from '../../1187598002/system/system.initialize.js' //初始化 - 系统模块
+import { systemArtificialTask } from '../../1187598002/system/system.artificialTask.js' //人工任务队列-系统模块
+
+//孵化器名
+global.spawnName = {
+    'W49S53': ['Spawn1'],
+    //W34N48: ['Spawn'],
+    //W33N49: ['Spawn1'],
+    //W32N45: ['Spawn-W32N45-1']
+};
+
+// 入口
+export default function () {
+    //初始化
+    systemInitialize.run();
+
+    //运行人工任务队列
+    systemArtificialTask.run();
+}
--- src/Alliance/main/main.run.Stars22.js
+++ src/Alliance/main/main.run.Stars22.js
@@ -1,23 +1,4 @@
-//var systemInitialize = require('./system/system.initialize'); //初始化 - 系统模块
-//var systemArtificialTask = require('./system/system.artificialTask'); //人工任务队列-系统模块
-
-//import { systemInitialize } from './system/system.initialize' //初始化 - 系统模块
-
-import { systemInitialize } from '../../1187598002/system/system.initialize.js' //初始化 - 系统模块
-import { systemArtificialTask } from '../../1187598002/system/system.artificialTask.js' //人工任务队列-系统模块
-
-//孵化器名
-global.spawnName = {
-    'W49S53': ['Spawn1'],
-    //W34N48: ['Spawn'],
-    //W33N49: ['Spawn1'],
-    //W32N45: ['Spawn-W32N45-1']
-};
-
+import main from '../../1187598002/main.run.js'
 export default function (_this, objectFun) {
-    //初始化
-    systemInitialize.run();
-
-    //运行人工任务队列
-    systemArtificialTask.run();
+    main();
 }
