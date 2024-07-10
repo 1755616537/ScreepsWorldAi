b7bde776526dcbe1f8edc6a7020327dbe9590418
1755616537 <1755616537@qq.com>
2024-07-10 23:28:14 +0800

    no message

--- src/globalData.js
+++ src/globalData.js
@@ -39,7 +39,9 @@ global.globalData = {
                 'W2N2': {
                     // 【可以不填写，会自动获取】自动安全(受到攻击自动开启)
                     AutomaticSecurity: false,
-                }
+                },
+                'W59S44':{},
+                'W35S42':{}
             }
         },
         'Stars22': {}
@@ -125,7 +127,7 @@ global.globalData = {
                 list: [WORK, WORK, WORK, CARRY, MOVE],
                 totalEnergyRequired: 300
             },
-            number: 1,
+            number: 0,
             // 自动缩减配置,当房间内总容量不足时
             AutomaticConfigurationDownsizing: true
         },
@@ -165,7 +167,7 @@ global.globalData = {
                 list: [WORK, WORK, WORK, CARRY, MOVE],
                 totalEnergyRequired: 300
             },
-            number: 1,
+            number: 0,
             // tower创建拥有能量的时候不生成维修者
             onTower: false,
             // 自动缩减配置,当房间内总容量不足时
