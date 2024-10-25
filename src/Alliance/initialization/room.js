3025c279f1a28129aa9235fe998cc8a1082a71ac
1755616537 <1755616537@qq.com>
2024-10-25 21:57:23 +0800

    临时命令执行

--- src/Alliance/initialization/room.js
+++ src/Alliance/initialization/room.js
@@ -6,6 +6,16 @@ export default [
             _.forEach(globalData.rooms, (room, roomName) => {
                 objectFun.iniRoom(roomName);
             })
+
+            // 临时命令执行
+            // console.log(Game.creeps['carrier63253954'].memory.SpecialActions={
+            //     code:'carrier3', msg:{
+            //         x:21,
+            //         y:31,
+            //         roomName:'W48S52'
+            //     }
+            // })
+            // console.log(Game.rooms['W48S54'].terminal.send(RESOURCE_ENERGY, 100000, 'W49S54'))
         }
     }
 ]
