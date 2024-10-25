51a5de0a0013b71a497c46df6cb72bd7b4d6446f
1755616537 <1755616537@qq.com>
2024-10-25 22:04:45 +0800

    临时命令执行

--- src/Alliance/initialization/room.js
+++ src/Alliance/initialization/room.js
@@ -8,14 +8,23 @@ export default [
             })
 
             // 临时命令执行
-            // console.log(Game.creeps['carrier63253954'].memory.SpecialActions={
-            //     code:'carrier3', msg:{
-            //         x:21,
-            //         y:31,
-            //         roomName:'W48S52'
-            //     }
-            // })
-            // console.log(Game.rooms['W48S54'].terminal.send(RESOURCE_ENERGY, 100000, 'W49S54'))
+            // 搬运能量('harvester1', 25, 25, 'W48S54')
+            // 终端发送能量('W48S54', 'W49S54', 100000)
         }
     }
 ]
+
+
+function 搬运能量(爬爬名,需要搬运的建筑x坐标,需要搬运的建筑y坐标,需要搬运的房间名){
+    console.log(Game.creeps[爬爬名].memory.SpecialActions={
+        code:'carrier3', msg:{
+            x:需要搬运的建筑x坐标,
+            y:需要搬运的建筑y坐标,
+            roomName:需要搬运的房间名
+        }
+    })
+}
+
+function 终端发送能量(需要发送的房间名,需要接收的房间名,数量){
+    console.log(Game.rooms[需要发送的房间名].terminal.send(RESOURCE_ENERGY, 数量, 需要接收的房间名))
+}
